1cb4a98e37a6376dbb4e587ac60f1e9196b89ac5
Mikhail Buchnev <me@buchnev.page>
2022-09-08 10:16:34 +0300

    Update metadata

--- setup.py
+++ setup.py
@@ -7,13 +7,25 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='console_logger',
-    version='0.0.7',
-    packages=['console_logger'],
-    url='https://github.com/BuchnevM/console_logger',
+    version='0.0.8',
+    packages=[
+        'console_logger',
+    ],
+    project_urls={
+        'Source': 'https://github.com/m1buchnev/console_logger',
+    },
     license='MIT License',
     author='Mikhail Buchnev',
     author_email='me@buchnev.page',
     description='Yet another logging package for Python',
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='log, logging, logger, console',
+    python_requires=">=3.7",
 )
